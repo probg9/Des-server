eea4eb7e6bb990f0e73f628c72b1b58fc55b299a
Astra <mrxastra@gmail.com>
2025-02-10 23:49:06 +0530

    first

--- src/pages/Payments-Update.jsx
+++ src/pages/Payments-Update.jsx
@@ -23,7 +23,7 @@ export const PaymentsUpdate = () => {
   const getSingleUserData = async () => {
     try {
       const response = await fetch(
-        `http://localhost:5500/api/admin/payments/${params.id}`,
+        `https://des-zeta.vercel.app/api/admin/payments/${params.id}`,
         {
           method: "GET",
           headers: {
@@ -57,7 +57,7 @@ export const PaymentsUpdate = () => {
 
     try {
       const response = await fetch(
-        `http://localhost:5500/api/admin/payments/update/${params.id}`,
+        `https://des-zeta.vercel.app/api/admin/payments/update/${params.id}`,
         {
           method: "PATCH",
           headers: {
--- src/pages/RecordUpdate.jsx
+++ src/pages/RecordUpdate.jsx
@@ -139,7 +139,7 @@ export const RecordUpdate = () => {
   const getSingleUserData = async () => {
     try {
       const response = await fetch(
-        `http://localhost:5500/api/admin/records/${params.id}`,
+        `https://des-zeta.vercel.app/api/admin/records/${params.id}`,
         {
           method: "GET",
           headers: {
@@ -532,7 +532,7 @@ export const RecordUpdate = () => {
       formData.append("AnkleRange", user.AnkleRange);
       formData.append("Additional_remarks", user.Additional_remarks);
 
-      const response = await fetch("http://localhost:5500/api/dataform/form", {
+      const response = await fetch("https://des-zeta.vercel.app/api/dataform/form", {
         method: "POST",
         body: formData,
       });
